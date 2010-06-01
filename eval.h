8dbc375e4304bc4a2f47a89585db4bdf86a4ea2c
l0stman <endian.sign@gmail.com>
2010-06-01 12:12:47 +0300

    Redefine everr as macro using raise.

--- eval.h
+++ eval.h
@@ -5,14 +5,6 @@ extern const excpt_t eval_error;
 extern exp_t *eval(exp_t *, env_t *);
 extern exp_t *apply(exp_t *, exp_t *, env_t *);
 
-/* Print the message and return a null pointer */
-static inline exp_t *
-everr(const char *msg, const exp_t *ep)
-{
-        char *s;
+#define everr(msg, ep)	raise(&eval_error,filename,linenum,msg" %s",tostr(ep))
 
-        warnx("%s: %s", msg, s = tostr(ep));
-        free(s);
-        return NULL;
-}
 #endif /* !EVAL_H */
