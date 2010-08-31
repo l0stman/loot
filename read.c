beaf25198df0c615f8bbfc5ec0899e2a7bbbbe8a
l0stman <endian.sign@gmail.com>
2010-09-01 02:55:41 +0300

    Define a syntax table and redefine the quotes expressions as independent functions.

--- read.c
+++ read.c
@@ -5,6 +5,115 @@
 
 const excpt_t read_error = { "read" };
 
+static exp_t *read_atm(FILE *, int);
+static exp_t *read_pair(FILE *);
+static exp_t *read_char(FILE *);
+static exp_t *read_quote(FILE *);
+static exp_t *read_qquote(FILE *);
+static exp_t *read_splice(FILE *);
+static exp_t *read_unquote(FILE *);
+
+/* Syntax table. */
+static exp_t *(*stab[128])() = {
+        /* 0  NUL */ NULL,
+        /* 1  SOH */ NULL,
+        /* 2  STX */ NULL,
+        /* 3  ETX */ NULL,
+        /* 4  EOT */ NULL,
+        /* 5  ENQ */ NULL,
+        /* 6  ACK */ NULL,
+        /* 7  BEL */ NULL,
+        /* 8  BS  */ NULL,
+        /* 9  HT  */ NULL,
+        /* 10 NL  */ NULL,
+        /* 11 VT  */ NULL,
+        /* 12 NP  */ NULL,
+        /* 13 CR  */ NULL,
+        /* 14 SO  */ NULL,
+        /* 15 SI  */ NULL,
+        /* 16 DLE */ NULL,
+        /* 17 DC1 */ NULL,
+        /* 18 DC2 */ NULL,
+        /* 19 DC3 */ NULL,
+        /* 20 DC4 */ NULL,
+        /* 21 NAK */ NULL,
+        /* 22 SYN */ NULL,
+        /* 23 ETB */ NULL,
+        /* 24 CAN */ NULL,
+        /* 25 EM  */ NULL,
+        /* 26 SUB */ NULL,
+        /* 27 ESC */ NULL,
+        /* 28 FS  */ NULL,
+        /* 29 GS  */ NULL,
+        /* 30 RS  */ NULL,
+        /* 31 US  */ NULL,
+        /* 32 SP  */ NULL,
+        /* 33 !   */ NULL,
+        /* 34 "   */ NULL,
+        /* 35 #   */ NULL,
+        /* 36 $   */ NULL,
+        /* 37 %   */ NULL,
+        /* 38 &   */ NULL,
+        /* 39 '   */ NULL,
+        /* 40 (   */ read_pair,
+        /* 41 )   */ NULL,
+        /* 42 *   */ NULL,
+        /* 43 +   */ NULL,
+        /* 44 ,   */ NULL,
+        /* 45 -   */ NULL,
+        /* 46 .   */ NULL,
+        /* 47 /   */ NULL,
+        /* 48 0   */ NULL,
+        /* 49 1   */ NULL,
+        /* 50 2   */ NULL,
+        /* 51 3   */ NULL,
+        /* 52 4   */ NULL,
+        /* 53 5   */ NULL,
+        /* 54 6   */ NULL,
+        /* 55 7   */ NULL,
+        /* 56 8   */ NULL,
+        /* 57 9   */ NULL,
+        /* 58 :   */ NULL,
+        /* 59 ;   */ NULL,
+        /* 60 <   */ NULL,
+        /* 61 =   */ NULL,
+        /* 62 >   */ NULL,
+        /* 63 ?   */ NULL,
+        /* 64 @   */ NULL,
+        /* 65 A   */ NULL,
+        /* 66 B   */ NULL,
+        /* 67 C   */ NULL,
+        /* 68 D   */ NULL,
+        /* 69 E   */ NULL,
+        /* 70 F   */ NULL,
+        /* 71 G   */ NULL,
+        /* 72 H   */ NULL,
+        /* 73 I   */ NULL,
+        /* 74 J   */ NULL,
+        /* 75 K   */ NULL,
+        /* 76 L   */ NULL,
+        /* 77 M   */ NULL,
+        /* 78 N   */ NULL,
+        /* 79 O   */ NULL,
+        /* 80 P   */ NULL,
+        /* 81 Q   */ NULL,
+        /* 82 R   */ NULL,
+        /* 83 S   */ NULL,
+        /* 84 T   */ NULL,
+        /* 85 U   */ NULL,
+        /* 86 V   */ NULL,
+        /* 87 W   */ NULL,
+        /* 88 X   */ NULL,
+        /* 89 Y   */ NULL,
+        /* 90 Z   */ NULL,
+        /* 91 [   */ NULL,
+        /* 92 \   */ NULL,
+        /* 93 ]   */ NULL,
+        /* 94 ^   */ NULL,
+        /* 95 _   */ NULL,
+        /* 96 `   */ NULL
+};
+
 /* skip spaces in the input stream. */
 static inline void
 skip_spa(FILE *fp)
@@ -48,11 +157,6 @@ skip(FILE *fp)
                 }
 }
 
-static exp_t *read_atm(FILE *, int);
-static exp_t *read_pair(FILE *);
-static exp_t *read_quote(FILE *, exp_t *);
-static exp_t *read_char(FILE *);
-
 #define doterr(line)	raise(&read_error, filename, line, "Illegal use of .")
 #define eoferr()	RAISE(read_error, "unexpected end of file")
 #define readerr(fmt, s) raise(&read_error, filename, linenum, fmt, s)
@@ -79,10 +183,10 @@ read(FILE *fp)
                 RAISE(read_error, "unexpected )");
                 break;
         case '\'':              /* quoted expression */
-                exp = read_quote(fp, keywords[QUOTE]);
+                exp = read_quote(fp);
                 break;
         case '`':
-                exp = read_quote(fp, keywords[QQUOTE]);
+                exp = read_qquote(fp);
                 break;
         case ',':
                 switch (c = fgetc(fp)) {
@@ -90,11 +194,11 @@ read(FILE *fp)
                         eoferr();
                         break;
                 case '@':
-                        exp = read_quote(fp, keywords[SPLICE]);
+                        exp = read_splice(fp);
                         break;
                 default:
                         ungetc(c, fp);
-                        exp = read_quote(fp, keywords[UNQUOTE]);
+                        exp = read_unquote(fp);
                         break;
                 }
                 break;
@@ -253,12 +357,40 @@ read_atm(FILE *fp, int ch)
 }
 
 /* Enclose the following expression exp into (keyword exp). */
-static exp_t *
-read_quote(FILE *fp, exp_t *keyword)
+static inline exp_t *
+enclose(FILE *fp, exp_t *keyword)
 {
         exp_t *ep;
 
         if (!(ep = read(fp)))
                 eoferr();
         return cons(keyword, cons(ep, null));
+}
+
+/* Read a quote expression. */
+static exp_t *
+read_quote(FILE *fp)
+{
+        return enclose(fp, keywords[QUOTE]);
+}
+
+/* Read a quasi-quote expression. */
+static exp_t *
+read_qquote(FILE *fp)
+{
+        return enclose(fp, keywords[QQUOTE]);
+}
+
+/* Read an unquote expression. */
+static exp_t *
+read_unquote(FILE *fp)
+{
+        return enclose(fp, keywords[UNQUOTE]);
+}
+
+/* Read an unquote-splicing expression. */
+static exp_t *
+read_splice(FILE *fp)
+{
+        return enclose(fp, keywords[SPLICE]);
 }
