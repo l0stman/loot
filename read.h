06aac829e1f04aa3c2829600bc6cb4d90ae1e244
l0stman <endian.sign@gmail.com>
2010-11-09 09:13:19 +0300

    Remove unnecessary macro UNGETC.

--- read.h
+++ read.h
@@ -3,11 +3,6 @@
 
 #include "stream.h"
 
-#define UNGETC(c, fp)	do {                            \
-                if (c != EOF && ungetc(c, fp) == EOF)   \
-                        err_sys("ungetc");              \
-        } while (0)
-
 extern const excpt_t read_error;
 extern exp_t *read(stream *);
 
